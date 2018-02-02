ae70092295cd9cab82d13250d41966d19e34e9a1
Kyle Desktop <kylespresser@gmail.com>
2018-02-01 21:12:42 -0500

    finish react version

--- react/compare-react/src/components/Faq/Faq.js
+++ react/compare-react/src/components/Faq/Faq.js
@@ -1,7 +1,25 @@
 import React, { Component } from 'react';
 import './Faq.css';
+import axios from 'axios';
 
 class Faq extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      faqs: []
+    };
+  }
+
+  componentDidMount() {
+    axios.get('http://jsonplaceholder.typicode.com/posts')
+      .then(res => {
+        const faqs = res.data.slice(0, 10);
+        this.setState({ faqs});
+      })
+  }
+
   render() {
     return (
       <div>
@@ -10,18 +28,18 @@ class Faq extends Component {
             <h1 className="title">FAQ</h1>
             <h2 className="subtitle is-4">Bacon ipsum and so on and so forth</h2>
 
-            <div className="columns" v-if="faqs && faqs.length">
-              <div className="column is-one-third" v-for="faq of faqs">
-                <div className="card">
-                  <div className="card-content">
-                    <p className="title">{ faq.title }</p>
-                    <p className="answer">{ faq.body }</p>
+            <div className="columns">
+              {this.state.faqs.map(faq =>
+                <div className="column is-one-third">
+                  <div className="card">
+                    <div className="card-content">
+                      <p className="title">{ faq.title }</p>
+                      <p className="answer">{ faq.body }</p>
+                    </div>
                   </div>
                 </div>
-              </div>
+              )}
             </div>
-
-
           </section>
         </div>
       </div>
